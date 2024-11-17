adeffc31dc842bf8537a30469ab105f335898b06
yurim <y1551463@gmail.com>
2024-11-17 19:05:15 +0900

    persist() 엔티티 객체를 영속성 컨텍스트에 저장한다 해당 엔티티 객체는 영속 상태로 전환된다 영속 상태로 전환된 엔티티 객체는 1차 캐시에 저장되고 트랜잭션이 커밋되면 데이터베이스에 동기화된다 persist()는 객체를 "JPA 관리 상태"로 만드는 중요한 메서드

---
+++ src/main/java/jpa/CustomerJpaExam.java
@@ -0,0 +1,31 @@
+package jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpa.entity.Customer;
+
+public class CustomerJpaExam {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("customer-exam"); // EntityManager 를 얻어오기 위한 Factory 설정
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction(); // Transaction 을 가져온다
+
+        tx.begin();
+        try { // persist, commit 과정에서 예외가 발생하면 rollback
+            em.persist(Customer.sample());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+
+}
