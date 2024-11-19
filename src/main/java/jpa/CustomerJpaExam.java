23f9bd405d16429a4f2c00ad4718b1c454feed9e
yurim <y1551463@gmail.com>
2024-11-19 17:29:22 +0900

    영속 상태인 객체를 find() 하면 1차 캐시에 등록된 객체를 반환하므로 조회 쿼리는 발생하지 않는다

--- src/main/java/jpa/CustomerJpaExam.java
+++ src/main/java/jpa/CustomerJpaExam.java
@@ -19,16 +19,13 @@ public class CustomerJpaExam {
         tx.begin();
         try { // persist, commit 과정에서 예외가 발생하면 rollback
 
-            Customer customer = new Customer("ID0004", "Lee"); // 순수 자바 객체 - 비영속 상태
+            Customer customer = new Customer("ID0005", "Jin"); // 순수 자바 객체 - 비영속 상태
             em.persist(customer); // 영속 객체 - 영속 상태 / find()를 통해 객체를 데이터베이스에서 가져와서 컨텍스트에 올리면 해당 객체도 영속 상태가 된다
 
-            Query query = em.createQuery("SELECT c FROM Customer c", Customer.class); // JPQL 쿼리 생성
-            // JPQL 을 날릴 때 flush 가 선행되기 때문에 해당 트랜잭션에서는 DB에 반영이 된 상태
+            Customer foundCustomer = em.find(Customer.class, "ID0005");
+            System.out.println(foundCustomer.toString());
 
-            List<Customer> customers = query.getResultList();
-            System.out.println(customers.toString());
-
-            //tx.commit(); 현재는 commit 하지 않음 -> 최종 DB 트랜잭션은 commit 하지 않았기 때문에 H2에서 바라볼 때는 반영되지 않음
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
